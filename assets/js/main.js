30fd66ab5d8052a576292af8a44fa681be456819
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-22 09:11:12 -0400

    En nuestro archivo js agregamos dos funciones, la primera para borrar los datos de los input por defecto y la otra para el autocompletado de estos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
+document.getElementById("partida").addEventListener("click",borrar);
+document.getElementById("destino").addEventListener("click",borrar);
+function borrar(){
+	document.getElementById("partida").value="";
+	document.getElementById("destino").value="";
+}
+
 function initMap(){
+
+	var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+    //Mostrar mapa
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 15,
 		center: {lat:  	-33.416667, lng:  -70.65}// Coordenadas de Recoleta
 	});
-	directionsDisplay.setMap(map);
+	//Autocompletado
+		directionsDisplay.setMap(map);
+		var inicio = (document.getElementById('partida')); 
+        var autocompletar = new google.maps.places.Autocomplete(inicio);
+        autocompletar.bindTo('bounds', map);          
+        var llegada = (document.getElementById('destino')); 
+        var autocompletar = new google.maps.places.Autocomplete(llegada);
+        autocompletar.bindTo('bounds', map);
 }
+
+	
